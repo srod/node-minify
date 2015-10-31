89fddf5d5973a42401b44e12ee753d2e88f501ac
Rodolphe Stoclin <srodolphe@gmail.com>
2015-10-31 23:03:36 +0100

    Separate compressors in modules

--- examples/server.js
+++ examples/server.js
@@ -6,22 +6,82 @@ http.createServer(function(req, res) {
   res.end('Hello World\n');
 }).listen(1337, '127.0.0.1');
 
-new compressor.minify({
-  type: 'gcc',
-  fileIn: 'public/js/**/*.js',
-  fileOut: 'public/js-dist/wildcards-match-gcc.js',
+compressor.minify({
+  type: 'yui-css',
+  fileIn: 'public/css/base.css',
+  fileOut: 'public/js-dist/yui-css.css',
   sync: true,
   callback: function(err, min) {
-    console.log('wildcards match GCC');
+    console.log('yui css');
     console.log(err);
     //console.log(min);
   }
 });
 
 compressor.minify({
+  type: 'sqwish',
+  fileIn: 'public/css/base2.css',
+  fileOut: 'public/js-dist/sqwish.css',
+  sync: true,
+  callback: function(err, min) {
+    console.log('sqwish');
+    console.log(err);
+    //console.log(min);
+  }
+});
+
+compressor.minify({
+  type: 'clean-css',
+  fileIn: 'public/css/base.css',
+  fileOut: 'public/js-dist/clean-css.css',
+  sync: true,
+  callback: function(err, min) {
+    console.log('clean-css');
+    console.log(err);
+    //console.log(min);
+  }
+});
+
+compressor.minify({
+  type: 'csso',
+  fileIn: 'public/css/base.css',
+  fileOut: 'public/js-dist/csso.css',
+  sync: true,
+  callback: function(err, min) {
+    console.log('csso');
+    console.log(err);
+    //console.log(min);
+  }
+});
+
+compressor.minify({
+  type: 'uglifyjs',
+  fileIn: 'public/js/base.js',
+  fileOut: 'public/js-dist/uglifyjs.js',
+  sync: true,
+  callback: function(err, min) {
+    console.log('uglify');
+    console.log(err);
+    //console.log(min);
+  }
+});
+
+compressor.minify({
+  type: 'no-compress',
+  fileIn: 'public/js/**/*.js',
+  fileOut: 'public/js-dist/no-compress.js',
+  sync: true,
+  callback: function(err, min) {
+    console.log('no-compress');
+    console.log(err);
+    //console.log(min);
+  }
+});
+
+/*compressor.minify({
   type: 'gcc',
   publicFolder: './public/',
-  //fileIn: '/js/**/*.js',
+  //fileIn: '/js/!**!/!*.js',
   fileIn: ['js/base.js', 'js/base2.js'],
   fileOut: 'public/js-dist/wildcards-match-gcc.js',
   sync: true,
@@ -35,10 +95,10 @@ compressor.minify({
 compressor.minify({
   type: 'gcc',
   publicFolder: './public/',
-  //fileIn: '/js/**/*.js',
+  //fileIn: '/js/!**!/!*.js',
   fileIn: 'js/base.js',
   fileOut: 'public/js-dist/wildcards-match-gcc.js',
   sync: true
-});
+});*/
 
 console.log('Server running at http://127.0.0.1:1337/');
